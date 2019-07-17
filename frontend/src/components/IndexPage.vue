dabb0ccefb794a24a14127df20602759c29be66a
Parkjunwoo <sinyoorang@gmail.com>
2019-07-18 01:17:01 +0900

    menu수정

--- frontend/src/components/IndexPage.vue
+++ frontend/src/components/IndexPage.vue
@@ -1,11 +1,29 @@
 <template>
-  <v-carousel hide-controls>
+  <!-- <v-carousel hide-controls>
     <v-carousel-item
       v-for="(movie,i) in movies"
       :key="i"
       :src="movie.poster"
     ></v-carousel-item>
-  </v-carousel>
+  </v-carousel> -->
+  <v-expansion-panel>
+    <v-expansion-panel-content
+      v-for="(movie,i) in movies"
+      :key="i"
+    >
+      <template v-slot:header>
+        <div>{{movie.name}}</div>
+      </template>
+      <v-card>
+        <v-img :src="movie.poster">
+        </v-img>
+        <v-card-text>
+          {{movie.description}}
+        </v-card-text>
+        
+      </v-card>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
 </template>
 
 <script>
