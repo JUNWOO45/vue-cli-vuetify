5d4e14017a1b030126969f838923eb71d1f4acd2
Parkjunwoo <sinyoorang@gmail.com>
2019-07-18 01:31:26 +0900

    이것저것..

--- frontend/src/components/IndexPage.vue
+++ frontend/src/components/IndexPage.vue
@@ -1,11 +1,4 @@
 <template>
-  <!-- <v-carousel hide-controls>
-    <v-carousel-item
-      v-for="(movie,i) in movies"
-      :key="i"
-      :src="movie.poster"
-    ></v-carousel-item>
-  </v-carousel> -->
   <v-expansion-panel>
     <v-expansion-panel-content
       v-for="(movie,i) in movies"
@@ -15,12 +8,51 @@
         <div>{{movie.name}}</div>
       </template>
       <v-card>
-        <v-img :src="movie.poster">
+        <v-img :src="movie.poster" max-width="800px">
         </v-img>
         <v-card-text>
           {{movie.description}}
+          <v-dialog
+            v-model="dialog"
+            width="500"
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn
+                color="red lighten-2"
+                dark
+                v-on="on"
+              >
+                이 영화 보고싶어요?
+              </v-btn>
+            </template>
+
+            <v-card>
+              <v-card-title
+                class="headline grey lighten-2"
+                primary-title
+              >
+                안돼요.
+              </v-card-title>
+
+              <v-card-text>
+                여울이가 크면 같이 봐요!
+              </v-card-text>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="primary"
+                  flat
+                  @click="dialog = false"
+                >
+                  I accept
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </v-card-text>
-        
       </v-card>
     </v-expansion-panel-content>
   </v-expansion-panel>
@@ -36,7 +68,8 @@ export default {
   },
   data () {
       return {
-        movies: []
+        movies: [],
+        dialog: false
       }
     }
 }
